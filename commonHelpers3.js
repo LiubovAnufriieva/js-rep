52c89e2e30e04be5a43b49725b818bb37375dbb1
LiubovAnufriieva <149685540+LiubovAnufriieva@users.noreply.github.com>
2024-05-07 08:12:32 +0000

    Deploying to gh-pages from @ LiubovAnufriieva/js-rep@d7ab77b7fed833573d4f20fd9673d530054c2fa0 🚀

--- commonHelpers3.js
+++ commonHelpers3.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                     */const t={output:document.querySelector(".js-output"),clearBtn:document.querySelector(".js-clear")};window.addEventListener("keydown",n);t.clearBtn.addEventListener("click",o);function n(e){t.output.textContent+=e.key}function o(){t.output.textContent=""}
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                     */const t={output:document.querySelector(".js-output"),clearBtn:document.querySelector(".js-clear")};window.addEventListener("keypress",n);t.clearBtn.addEventListener("click",o);function n(e){t.output.textContent+=e.key}function o(){t.output.textContent=""}
 //# sourceMappingURL=commonHelpers3.js.map
